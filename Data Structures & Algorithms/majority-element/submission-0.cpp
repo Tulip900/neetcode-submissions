1f7504bb18f03f517dbc010e7292a9be75474944
Tulip900 <tulipghosh593@gmail.com>
2026-09-11 00:30:14 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>freq;
+        for(int x:nums){
+            freq[x]++;
+        }
+        for(auto p:freq){
+            if(p.second>n/2){
+                return p.first;
+            }
+        }
+        return -1;
+    }
+};

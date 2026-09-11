525323e3853939b1cca1707353411bdbf937a8e3
Tulip900 <tulipghosh593@gmail.com>
2026-09-11 12:54:45 +0530

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static unordered_map<int,int>freq;
+    static bool cmp(int a,int b){
+        if(freq[a]!=freq[b]){
+            return freq[a]<freq[b];
+        }
+        return a>b;
+    }
+    vector<int> frequencySort(vector<int>& nums) {
+        freq.clear();
+        for(int x:nums){
+            freq[x]++;
+        }
+        sort(nums.begin(),nums.end(),cmp);
+        return nums;
+    }
+};
+unordered_map<int,int>Solution::freq;

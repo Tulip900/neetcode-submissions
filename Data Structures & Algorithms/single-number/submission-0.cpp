e6e506f9314cd2504bad491dbd3135100637bb91
Tulip900 <tulipghosh593@gmail.com>
2026-09-13 23:28:06 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int x:nums){
+            freq[x]++;
+        }
+        for(auto p:freq){
+            if(p.second==1){
+                return p.first;
+            }
+        }
+        return -1;
+    }
+};
